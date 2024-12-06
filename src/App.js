8bae4a704ebfd6aab897b7e39f1d8ff79ab97dd0
Sajan Chouhan <sajan.chouhan@happay.in>
2024-12-06 23:54:49 +0530

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import ToggleTheme from "./Component/ToggleTheme";
-
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline p-20">
-      <ToggleTheme />
-    </h1>
-  );
-}
